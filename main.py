999a8568411d0438cd8d30b96b200b7f32102b83
Cole <nicholas.carpenter.215@gmail.com>
2023-04-28 18:31:57 -0700

    First Draft

--- main.py
+++ main.py
@@ -1,1 +1,39 @@
-print("Hello World")
+import random
+
+num1 = 0
+num2 = 0
+output = 0
+choice = 0
+rolls = 0
+adv = "no"
+
+choice = input("Which die would you like to roll? ")
+
+if choice == "20":
+    num1 = random.randint(1,20)
+    num2 = random.randint(1,20)
+    adv = input("Do you have advantage? ")
+    if adv == "yes" or "Yes":
+        output = "You rolled a (" + str(num1) + " and a " + str(num2) +"). Leaving you with " + str(max(num1,num2))
+    else:
+        adv = input("Do you have disadvantage? ")
+        if adv == "yes" or "Yes":
+            output = "You rolled a (" + str(num1) + " and a " + str(num2) +"). Leaving you with " + str(min(num1,num2))
+        else:
+            output = "You rolled a " + str(num1)
+else:
+    rolls = input("How many dice would you like to roll? ")
+    i = 0
+    while i < int(rolls):
+        num1 = random.randint(1,int(choice))
+        if i == 0:
+            num2 = str(num1) + " "
+        else:
+            if i < (int(rolls)-1):
+                num2 = num2 + str(num1) + " "
+            else:
+                num2 = num2 + str(num1)
+        i += 1
+    output = "Your rolls are (" + num2 + ")"
+
+print(output)
